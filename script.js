d3e8e857de523fa5d9eedb30dac20c577a829ff2
SUTD-research-day <sutdresearchday@gmail.com>
2025-02-18 22:49:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const registerButton = document.getElementById("register-btn");
+    registerButton.addEventListener("click", function() {
+        alert("Registration is currently closed. Stay tuned for updates!");
+    });
+});
